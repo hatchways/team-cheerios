53130f1817eb0a615052d6a6ecde7dcced5c8c03
Althaf <althafahmed68@hotmail.com>
2020-11-10 18:45:02 -0500

    use cors and remove other routes

--- server/app.js
+++ server/app.js
@@ -3,24 +3,26 @@ const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
-const indexRouter = require("./routes/index");
-const pingRouter = require("./routes/ping");
-const uploadRouter = require("./routes/upload.js");
-
+const uploadRouter = require("./routes/upload");
+const getRouter = require("./routes/get")
 const { json, urlencoded } = express;
 
 var app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/ping", pingRouter);
-app.use("/upload", uploadRouter);
+
+
+app.use("/",uploadRouter);
+app.use(getRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
