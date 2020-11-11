5643860af4237741a0b5e0fbe57f1709310d5a37
Althaf <althafahmed68@hotmail.com>
2020-11-11 14:40:15 -0500

    resolve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PictureUpload from './pages/PictureUpload'
+import { UserProvider } from "./contexts/UserContext";
 import { theme } from "./themes/theme";
-
-import "./App.css";
+import PrivateRoute from "./utils/PrivateRoute";
+import routes from "./routes";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={PictureUpload} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <UserProvider>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+        <Route path="/picture" component={PictureUpload} />
+
+          <Switch>
+            {routes.map((route, i) =>
+              route.auth ? (
+                <PrivateRoute
+                  exact
+                  path={route.path}
+                  component={route.component}
+                  key={`route-${i}`}
+                />
+              ) : (
+                <Route
+                  exact
+                  path={route.path}
+                  component={route.component}
+                  key={`route-${i}`}
+                />
+              )
+            )}
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </UserProvider>
   );
 }
 
