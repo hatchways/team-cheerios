9ca5eeed9235c9734bbc2d7f4d549288472818d8
Althaf <althafahmed68@hotmail.com>
2020-11-17 20:33:55 -0500

    integrate with frontend

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,17 +1,130 @@
 import React from "react";
+import axios from "axios";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { useHistory } from "react-router-dom";
 
-import { UserContext } from "../contexts/UserContext";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-export default function Signup() {
-  const { state } = React.useContext(UserContext);
+export default function SignUp() {
+  const [name, setName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const classes = useStyles();
   let history = useHistory();
 
-  if (state.authenticated) history.push("/");
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log(name);
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    //console.log(newUser);
+    axios
+      .post("/api/users",user)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error.response));
+    
+    history.push("/login");
+  };
 
   return (
-    <div>
-      <h1>This is our Signup page.</h1>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="Name"
+                variant="outlined"
+                required
+                fullWidth
+                value={name}
+                id="name"
+                label="Name"
+                autoFocus
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                value={password}
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSignup}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
