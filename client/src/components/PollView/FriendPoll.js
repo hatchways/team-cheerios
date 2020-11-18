bf1b20ab3cac4c494e4ee5a3889ac3a42dbd6264
Yoni <yoniakabecky@gmail.com>
2020-11-17 22:04:07 -0800

    Create PollView

---
+++ client/src/components/PollView/FriendPoll.js
@@ -0,0 +1,53 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+
+dayjs.extend(relativeTime);
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    borderTop: "1px solid #E8EBF6",
+    padding: "0 2rem",
+    maxWidth: 600,
+    height: 100,
+  },
+  friendName: {
+    flexGrow: 1,
+    paddingLeft: "1rem",
+  },
+  votedImage: {
+    width: 60,
+    height: 60,
+    objectFit: "cover",
+  },
+}));
+
+export default function FriendPoll({ name, image, vote, votedAt, images }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Avatar alt={name} src={image} />
+
+      <div className={classes.friendName}>
+        <Typography variant="h6">{name}</Typography>
+
+        <Typography variant="body2" color="textSecondary">
+          {dayjs(votedAt).fromNow()}
+        </Typography>
+      </div>
+
+      <img
+        src={vote === "a" ? images[0] : images[1]}
+        alt={`${name}'s choice`}
+        className={classes.votedImage}
+      />
+    </div>
+  );
+}
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,34 +1,70 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+
+import { NAVBAR_HEIGHT } from "../utils/constants";
 import DashboardLayout from "../components/DashboardLayout";
-import FriendsListDialog from "../components/FriendsListDialog";
-import OutlinedBtn from "../components/OutlinedBtn";
+import ProfileView from "../components/ProfileView";
+import PollView from "../components/PollView";
 import DashboardSkeleton from "../components/Skeletons/DashboardSkeleton";
 import { UserContext } from "../contexts/UserContext";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    background: "#FAFBFE",
+    height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+  },
+  leftPane: {
+    padding: "2.5rem",
+    background: "#FFFFFF",
+    height: "100%",
+    overflow: "scroll",
+  },
+  rightPane: {
+    flexGrow: 1,
+    padding: "2.5rem",
+    height: "100%",
+    overflow: "scroll",
+  },
+}));
+
 export default function Dashboard() {
+  const classes = useStyles();
   const {
     state: { loading },
   } = React.useContext(UserContext);
-  const [openFriendDialog, setOpenFriendDialog] = React.useState(true);
 
   return (
     <DashboardLayout>
       {loading ? (
         <DashboardSkeleton />
       ) : (
-        <div>
-          <h1>Dashboard</h1>
+        <Grid container className={classes.root}>
+          <Box
+            component={Grid}
+            item
+            sm={4}
+            md={3}
+            className={classes.leftPane}
+            display={{ xs: "none", sm: "block" }}
+          >
+            Friends
+          </Box>
 
-          <OutlinedBtn onClick={() => setOpenFriendDialog(true)}>
-            Create List
-          </OutlinedBtn>
-
-          <FriendsListDialog
-            open={openFriendDialog}
-            onClose={() => setOpenFriendDialog(false)}
-          />
-        </div>
+          <Grid item sm={8} md={9} className={classes.rightPane}>
+            <Switch>
+              <Route exact path="/dashboard" render={() => <ProfileView />} />
+              <Route
+                exact
+                path="/dashboard/poll/:pollId"
+                render={({ match }) => <PollView id={match.params.pollId} />}
+              />
+            </Switch>
+          </Grid>
+        </Grid>
       )}
     </DashboardLayout>
   );
