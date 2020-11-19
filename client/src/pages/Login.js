758a8bc5819b556cc871ed260fefb9c94db1a52e
Althaf <althafahmed68@hotmail.com>
2020-11-18 19:53:10 -0500

    sign up and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,68 @@
 import axios from "axios";
 import React from "react";
 import { useHistory } from "react-router-dom";
-
 import { SET_AUTHENTICATED } from "../contexts/types";
 import { UserContext } from "../contexts/UserContext";
+import {
+  Grid,
+  Paper,
+  TextField,
+  Link,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Logo from "../components/Logo";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://i.ibb.co/YRBjvKT/f1d2e32ad77c9c983af281c12eee46567109a4f6.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    position : "absolute",
+    marginTop : "2rem",
+    marginLeft: "2rem",
+  },
+  form: {
+    width: "50%", // Fix IE 11 issue.
+    marginTop: theme.spacing(5),
+    marginLeft: theme.spacing(1),
+  },
+  submit: {
+    margin: "1rem auto",
+    borderRadius: 25,
+    background: "#000000",
+    padding: "0 1.5rem",
+    height: 50,
+    width: 150,
+    color: "#FFFFFF",
+    "&:hover": {
+      border: "1px solid",
+      background: "unset",
+      color: "unset",
+    },
+  },
+}));
 
 export default function Login() {
+  const classes = useStyles();
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const { state, dispatch } = React.useContext(UserContext);
@@ -27,25 +84,63 @@ export default function Login() {
   };
 
   return (
-    <div style={{ height: "100vh" }}>
-      <h1>This is our Login page.</h1>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
 
-      <input
-        id="email"
-        type="email"
-        value={email}
-        placeholder="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        id="password"
-        type="password"
-        value={password}
-        placeholder="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleClick}>Login</button>
-    </div>
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+        <Logo className = {classes.avatar}/>
+          <Typography component="h1" variant="h3">
+            Log In
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick = {handleClick}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+      <Grid item xs={false} sm={1} md={7} className={classes.image} />
+    </Grid>
   );
 }
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,36 +1,62 @@
 import React from "react";
 import axios from "axios";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import { useHistory } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Logo from "../components/Logo"
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://i.ibb.co/YRBjvKT/f1d2e32ad77c9c983af281c12eee46567109a4f6.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
   paper: {
-    marginTop: theme.spacing(8),
+    margin: theme.spacing(8, 4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    position : "absolute",
+    marginTop : "2rem",
+    marginLeft: "2rem",
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    width: "50%", // Fix IE 11 issue.
+    marginTop: theme.spacing(5),
+    marginLeft: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: "1rem auto",
+    borderRadius: 25,
+    background: "#000000",
+    padding: "0 1.5rem",
+    height: 50,
+    width: 150,
+    color: "#FFFFFF",
+    "&:hover": {
+      border: "1px solid",
+      background: "unset",
+      color: "unset",
+    },
   },
 }));
+;
 
 export default function SignUp() {
   //const { state, dispatch } = React.useContext(UserContext);
@@ -57,74 +83,74 @@ export default function SignUp() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <Grid container component="main" className={classes.root}>
+    <CssBaseline />
+
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
+      <Logo className = {classes.avatar}/>
+        <Typography component="h1" variant="h3">
+          Create an account
         </Typography>
-        <form className={classes.form}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                name="Name"
-                variant="outlined"
-                required
-                fullWidth
-                value={name}
-                id="name"
-                label="Name"
-                autoFocus
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                value={password}
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Grid>
-          </Grid>
+        <form className={classes.form} noValidate>
+        <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            value={password}
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={handleSignup}
+            onClick = {handleSignup}
           >
-            Sign Up
+            Create
           </Button>
-          <Grid container justify="flex-end">
+          <Grid container>
             <Grid item>
               <Link href="/login" variant="body2">
-                Already have an account? Sign in
+                {"Already have an account? Sign In"}
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-    </Container>
+    </Grid>
+    <Grid item xs={false} sm={1} md={7} className={classes.image} />
+  </Grid>
   );
 }
