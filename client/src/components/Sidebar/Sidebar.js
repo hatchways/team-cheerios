7550c50466012af9a42e5a5adb023bc44d06e2a2
Yoni <yoniakabecky@gmail.com>
2020-11-19 18:11:32 -0800

    Delete unnecessary import

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
-import { UserContext } from "../../contexts/UserContext";
 import User from "../User";
 
 const useStyles = makeStyles((theme) => ({
