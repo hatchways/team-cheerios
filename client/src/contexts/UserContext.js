c66889ddf73dc3d6f35db3805438fca273bf1ddd
Yoni <yoniakabecky@gmail.com>
2020-11-23 12:53:09 -0800

    Add token check

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 
 import {
@@ -17,31 +18,51 @@ const initialState = {
 
 export const UserContext = React.createContext(initialState);
 
+const UserReducer = (state, action) => {
+  switch (action.type) {
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SET_USER:
+      return {
+        authenticated: true,
+        loading: false,
+        ...action.payload,
+      };
+    case SET_AUTHENTICATED:
+      return {
+        ...state,
+        authenticated: true,
+      };
+    case SET_UNAUTHENTICATED:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const token = localStorage.HatchwayToken;
+
 export const UserProvider = ({ children }) => {
-  const [state, dispatch] = React.useReducer((state, action) => {
-    switch (action.type) {
-      case LOADING_USER:
-        return {
-          ...state,
-          loading: true,
-        };
-      case SET_USER:
-        return {
-          authenticated: true,
-          loading: false,
-          ...action.payload,
-        };
-      case SET_AUTHENTICATED:
-        return {
-          ...state,
-          authenticated: true,
-        };
-      case SET_UNAUTHENTICATED:
-        return initialState;
-      default:
-        return state;
+  const [state, dispatch] = React.useReducer(UserReducer, initialState);
+
+  React.useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["x-auth-token"] = token;
+
+      axios
+        .get("/api/users/me")
+        .then(({ data: { name, image, email } }) => {
+          dispatch({
+            type: SET_USER,
+            payload: { user: { name, image, email } },
+          });
+        })
+        .catch((err) => console.error(err));
     }
-  }, initialState);
+  }, []);
 
   return (
     <UserContext.Provider value={{ state, dispatch }}>
