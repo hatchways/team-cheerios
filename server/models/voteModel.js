9e69543af15b78e2c5461c21e57fc1928ab4de05
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2020-11-24 01:25:30 -0800

    BE: Vote (#45)
    * Change model name to FriendsList
    
    * Update FriendsList route and controllers
    
    * Update Poll routes and controllers
    
    * Change friendsList model
    
    * Update friendsListControllers.js
    
    * Update friendsList
    
    * Fetch friendsList
    
    * Update auth.js
    
    * Integrate create poll
    
    * Clean up
    
    * Create Vote model and controllers
    
    * Reduce repitition

---
+++ server/models/voteModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const voteSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    pollId: {
+      type: Schema.Types.ObjectId,
+      ref: "Poll",
+    },
+    voteFor: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Vote", voteSchema);
