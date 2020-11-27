6a2e44da76714b8b389b02e00de53350e27fa3d9
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2020-11-27 13:00:15 -0800

    Poll Page Integration (#48)
    * Int poll page and voting
    
    * Create a func for fetching poll

--- client/src/apis/poll.js
+++ client/src/apis/poll.js
@@ -2,13 +2,11 @@ import axios from "axios";
 
 import uploadtoS3 from "../utils/uploadtoS3";
 
-const BASE_URL = "/poll";
-
 export const createNewPoll = (files, newPoll) =>
   uploadtoS3(files)
     .then((imageUrls) =>
       axios
-        .post(BASE_URL, { images: imageUrls, ...newPoll })
+        .post("/poll", { images: imageUrls, ...newPoll })
         .then((res) => res.data)
     )
     .catch((err) => console.error(err));
@@ -18,3 +16,30 @@ export const getPolls = () =>
     .get("/poll")
     .then((res) => res.data)
     .catch((err) => console.log(err));
+
+export const getPollById = (pollId) =>
+  axios
+    .get(`/poll/${pollId}`)
+    .then((res) => res.data)
+    .catch((err) => console.error(err));
+
+const VOTED_MSG = "You already voted";
+const SAME_VOTE_MSG = "You already vote for that choice";
+
+export const voteForPoll = (pollId, choice) =>
+  axios
+    .post(`/poll/${pollId}/vote/${choice}`)
+    .then((res) => {
+      if (res.data.message === VOTED_MSG) return false;
+      return true;
+    })
+    .catch((err) => console.error(err));
+
+export const changeYourVote = (pollId, choice) =>
+  axios
+    .post(`/poll/${pollId}/change/${choice}`)
+    .then((res) => {
+      if (res.data.message === SAME_VOTE_MSG) return false;
+      return true;
+    })
+    .catch((err) => console.error(err));
