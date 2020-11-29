ba11fbb4fb8729f4e8d258cd9133bc9381ef2f12
Yoni <yoniakabecky@gmail.com>
2020-11-29 01:58:00 -0800

    Organize login

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -22,6 +22,16 @@ export const checkLoggedIn = (dispatch) => {
   } else {
     dispatch({ type: SET_UNAUTHENTICATED });
   }
+};
+
+export const loginUser = async (user) => {
+  try {
+    const res = await axios.post("/api/auth", user);
+    localStorage.setItem(TOKEN_KEY, res.data.token);
+    axios.defaults.headers.common["x-auth-token"] = res.data.token;
+    return res.data.user;
+  } catch (err) {
+    console.error(err);
   }
 };
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -18,7 +18,7 @@ import Background from "../assets/login_bg.png";
 import Logo from "../components/Logo";
 import { SET_USER } from "../contexts/types";
 import { UserContext } from "../contexts/UserContext";
-import loginUser from "../utils/loginUser";
+import { loginUser } from "../apis/user";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -113,17 +113,11 @@ export default function Login() {
       loginUser(newUser)
         .then((user) => {
           if (user) {
-            const name = user.name;
-            const image = user.image;
-            const email = user.email;
+            const { name, image, email } = user;
             dispatch({
               type: SET_USER,
               payload: {
-                user: {
-                  name,
-                  image,
-                  email,
-                },
+                user: { name, image, email },
               },
             });
             history.push("/dashboard");
