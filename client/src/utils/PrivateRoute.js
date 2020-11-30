08ac509a5c637511299087958df20e8519113333
Althaf <althafahmed68@hotmail.com>
2020-11-30 16:43:40 -0500

    can refresh page now

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -6,13 +6,16 @@ import { UserContext } from "../contexts/UserContext";
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { state } = React.useContext(UserContext);
   const authenticated = state.authenticated ?? false;
+  const loading = state.loading;
 
   return (
+    !loading?
     <Route
       {...rest}
       render={(props) =>
         authenticated ? <Component {...props} /> : <Redirect to="/" />
       }
-    />
+    /> 
+    : ""
   );
 }
