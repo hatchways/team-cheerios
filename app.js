c2c26867336e08be7f99c91dca573570fd4b703e
Althaf <althafahmed68@hotmail.com>
2020-12-17 23:12:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,11 +39,6 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
