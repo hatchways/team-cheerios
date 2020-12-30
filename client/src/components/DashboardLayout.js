9589fe1e1e84ee12eccedf5cdefacd1c81c5ce04
Yoni <yoniakabecky@gmail.com>
2020-12-29 20:37:05 -0800

    Responsive navbar

--- client/src/components/DashboardLayout.js
+++ client/src/components/DashboardLayout.js
@@ -1,24 +1,32 @@
 import React from "react";
 
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { NAVBAR_HEIGHT } from "../utils/constants";
+import {
+  NAVBAR_HEIGHT,
+  MOBILE_NAV_HEIGHT,
+  BOTTOM_NAV_HEIGHT,
+} from "../utils/constants";
 import Navbar from "./Navbar";
+import BottomNav from "./BottomNav";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      width: "100vw",
-      height: "100vh",
-      overflow: "hidden",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+  },
+  children: {
+    display: "flex",
+    marginTop: NAVBAR_HEIGHT,
+    height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+
+    [theme.breakpoints.down("sm")]: {
+      marginTop: MOBILE_NAV_HEIGHT,
+      height: `calc(100vh - ${MOBILE_NAV_HEIGHT + BOTTOM_NAV_HEIGHT}px)`,
     },
-    children: {
-      display: "flex",
-      marginTop: NAVBAR_HEIGHT,
-      height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
-    },
-  })
-);
+  },
+}));
 
 export default function DashboardLayout({ children, ...props }) {
   const classes = useStyles();
@@ -30,6 +38,8 @@ export default function DashboardLayout({ children, ...props }) {
       <div className={classes.children} {...props}>
         {children}
       </div>
+
+      <BottomNav />
     </div>
   );
 }
--- client/src/components/Navbar/ProfileBtn.js
+++ client/src/components/Navbar/ProfileBtn.js
@@ -1,39 +1,44 @@
 import React from "react";
 
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 
 import UserSkeleton from "../Skeletons/UserSkeleton";
 import ProfileMenu from "./ProfileMenu";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      alignItems: "center",
-      marginRight: theme.spacing(5),
-      marginLeft: theme.spacing(5),
-      cursor: "pointer",
-      height: 50,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    marginRight: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+    cursor: "pointer",
+    height: 50,
+    [theme.breakpoints.down("xs")]: {
+      margin: "0 auto",
     },
-    avatar: {
-      marginRight: theme.spacing(1),
-      width: 50,
-      height: 50,
+  },
+  avatar: {
+    marginRight: theme.spacing(1),
+    width: 50,
+    height: 50,
+  },
+  userName: {
+    fontSize: 18,
+    textTransform: "none",
+    color: "black",
+
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
     },
-    userName: {
-      fontSize: 18,
-      textTransform: "none",
-      color: "black",
-    },
-  })
-);
+  },
+}));
 
 export default function ProfileBtn({ user, loading, dispatch, ...rest }) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -56,7 +61,7 @@ export default function ProfileBtn({ user, loading, dispatch, ...rest }) {
         <span className={classes.userName}>{user.name}</span>
       </Button>
 
-      <ProfileMenu anchorEl={anchorEl} handleClose={handleClose}/>
+      <ProfileMenu anchorEl={anchorEl} handleClose={handleClose} />
     </>
   );
 }
