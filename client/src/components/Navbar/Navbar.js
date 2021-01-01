c75054e1c5e4274bff6e4fdacfa7e112af45c84a
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2020-12-31 16:35:19 -0800

    Responsive navbar (#78)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 
 import { UserContext } from "../../contexts/UserContext";
-import { NAVBAR_HEIGHT } from "../../utils/constants";
+import { NAVBAR_HEIGHT, MOBILE_NAV_HEIGHT } from "../../utils/constants";
+import PollBtn from "../PollDialog/PollBtn";
 import Logo from "../Logo";
 import MenuBtn from "./MenuBtn";
 import ProfileBtn from "./ProfileBtn";
-import PollBtn from "../PollDialog/PollBtn";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      boxShadow: "0px 0px 30px rgba(230, 235, 248, 0.4)",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: "0px 0px 30px rgba(230, 235, 248, 0.4)",
+  },
+  toolbar: {
+    background: "#FFFFFF",
+    minHeight: NAVBAR_HEIGHT,
+
+    [theme.breakpoints.down("sm")]: {
+      minHeight: MOBILE_NAV_HEIGHT,
     },
-    toolbar: {
-      background: "#FFFFFF",
-      minHeight: NAVBAR_HEIGHT,
+  },
+  logo: {
+    [theme.breakpoints.down("sm")]: {
+      height: 48,
+      width: 48,
     },
-    spacer: {
-      flexGrow: 1,
-    },
-  })
-);
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Navbar() {
   const classes = useStyles();
@@ -36,10 +44,15 @@ export default function Navbar() {
   if (!authenticated) return null;
 
   return (
-    <AppBar position="fixed" elevation={0} className={classes.root}>
+    <AppBar
+      position="fixed"
+      elevation={0}
+      className={classes.root}
+      color="transparent"
+    >
       <Toolbar className={classes.toolbar}>
         <Link to="/dashboard">
-          <Logo />
+          <Logo className={classes.logo} />
         </Link>
 
         <div className={classes.spacer} />
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,1 +1,3 @@
 export const NAVBAR_HEIGHT = 120;
+export const MOBILE_NAV_HEIGHT = 80;
+export const BOTTOM_NAV_HEIGHT = 54;
