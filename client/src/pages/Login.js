e374bbd8061d1cb7db66008dff98d624ee78efd4
Yoni <yoniakabecky@gmail.com>
2021-01-07 15:18:02 -0800

    Modified authentication pages for small screen

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,65 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-import {
-  Grid,
-  Paper,
-  TextField,
-  Link,
-  Button,
-  Snackbar,
-} from "@material-ui/core";
+import { TextField, Link, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import MuiAlert from "@material-ui/lab/Alert";
 
 import { loginUser } from "../apis/user";
-import Background from "../assets/login_bg.png";
-import Logo from "../components/Logo";
+import AuthPageLayout from "../components/AuthPageLayout";
 import { SET_USER } from "../contexts/types";
 import { UserContext } from "../contexts/UserContext";
 
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
 const useStyles = makeStyles((theme) => ({
-  snackbar: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-  root: {
-    height: "100vh",
-  },
-  image: {
-    backgroundImage: `url(${Background})`,
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    position: "absolute",
-    marginTop: "2rem",
-    marginLeft: "2rem",
-  },
-  form: {
-    width: "50%",
-    marginTop: theme.spacing(5),
-    marginLeft: theme.spacing(1),
-  },
   submit: {
     margin: "1rem auto",
     borderRadius: 25,
@@ -81,29 +31,33 @@ export default function Login() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [errEmail, setErrEmail] = React.useState("");
+  const [errPassword, setErrPassword] = React.useState("");
   const [open, setOpen] = React.useState(false);
+  const [reason, setReason] = React.useState("");
   const { dispatch } = React.useContext(UserContext);
   let history = useHistory();
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
-
   const validate = () => {
     let isError = false;
+    setErrEmail("");
+    setErrPassword("");
+
     if (email === "" || email.indexOf("@") === -1) {
       setErrEmail("Please enter a valid email");
       isError = true;
     }
+    if (password === "") {
+      setErrPassword("Please enter a valid password");
+      isError = true;
+    }
+
     return isError;
   };
 
   const handleClick = (e) => {
     e.preventDefault();
     const err = validate();
+
     if (!err) {
       const newUser = {
         email,
@@ -125,79 +79,61 @@ export default function Login() {
               socket.socket.emit("log in", _id);
             });
           } else {
+            setReason("Error ! Your email or password is incorrect !");
             setOpen(true);
           }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.error(err));
     }
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
+    <AuthPageLayout page="Log In" open={open} setOpen={setOpen} reason={reason}>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        error={errEmail !== ""}
+        helperText={errEmail}
+        autoFocus
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        value={password}
+        id="password"
+        error={errPassword !== ""}
+        helperText={errPassword}
+        autoComplete="current-password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={handleClick}
+      >
+        Sign In
+      </Button>
 
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Logo className={classes.avatar} />
-          <Typography component="h1" variant="h3">
-            Log In
-          </Typography>
-          <form className={classes.form}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              error={errEmail !== ""}
-              helperText={errEmail}
-              autoFocus
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              id="password"
-              autoComplete="current-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={handleClick}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <div className={classes.snackbar}>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="error">
-              Error ! Your email or password is incorrect !
-            </Alert>
-          </Snackbar>
-        </div>
-      </Grid>
-      <Grid item xs={false} sm={1} md={7} className={classes.image} />
-    </Grid>
+      <div>
+        <Link href="/signup" variant="body2">
+          {"Don't have an account? Sign Up"}
+        </Link>
+      </div>
+    </AuthPageLayout>
   );
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,56 +2,16 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import Snackbar from "@material-ui/core/Snackbar";
 import { makeStyles } from "@material-ui/core/styles";
-import MuiAlert from "@material-ui/lab/Alert";
 
-import Background from "../assets/login_bg.png";
 import { signup } from "../apis/user";
-import Logo from "../components/Logo";
+import AuthPageLayout from "../components/AuthPageLayout";
 import { SET_USER } from "../contexts/types";
 import { UserContext } from "../contexts/UserContext";
 
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-  },
-  image: {
-    backgroundImage: `url(${Background})`,
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    position: "absolute",
-    marginTop: "2rem",
-    marginLeft: "2rem",
-  },
-  form: {
-    width: "50%",
-    marginTop: theme.spacing(5),
-    marginLeft: theme.spacing(1),
-  },
   submit: {
     margin: "1rem auto",
     borderRadius: 25,
@@ -69,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignUp() {
+  const classes = useStyles();
   const [name, setName] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [rePassword, setRePassword] = React.useState("");
@@ -82,16 +43,8 @@ export default function SignUp() {
     retypePassErr: "",
     nameErr: "",
   });
-  const classes = useStyles();
-  let history = useHistory();
   const { passwordErr, emailErr, retypePassErr, nameErr } = validError;
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
+  let history = useHistory();
 
   const reset = () => {
     setValidError({
@@ -104,39 +57,31 @@ export default function SignUp() {
 
   const validate = () => {
     reset();
-    let isErr = false;
+    let errorMessage = {};
+
     if (email.indexOf("@") === -1 || email === "" || email.length < 5) {
-      setValidError({
-        ...validError,
-        emailErr: "Please enter a valid email with at least 5 characters",
-      });
-      isErr = true;
+      errorMessage.emailErr =
+        "Please enter a valid email with at least 5 characters";
     }
     if (password.length < 5) {
-      setValidError({
-        ...validError,
-        passwordErr: "Password should be at least 8 characters",
-      });
-      isErr = true;
+      errorMessage.passwordErr = "Password should be at least 8 characters";
     }
     if (password !== rePassword && password !== "") {
-      setValidError({ ...validError, retypePassErr: "Passwords do not match" });
-      isErr = true;
+      errorMessage.retypePassErr = "Passwords do not match";
     }
     if (name.length < 3 || name.length > 30) {
-      setValidError({
-        ...validError,
-        nameErr:
-          "Name should be at least 3 characters long. Maximum length of 30 allowed",
-      });
-      isErr = true;
+      errorMessage.nameErr =
+        "Name should be at least 3 characters long. Maximum length of 30 allowed";
     }
-    return isErr;
+
+    setValidError({ ...errorMessage });
+    return errorMessage === {};
   };
 
   const handleSignup = (e) => {
     e.preventDefault();
     const isErr = validate();
+
     if (!isErr) {
       const newUser = {
         name,
@@ -145,9 +90,9 @@ export default function SignUp() {
       };
 
       signup(newUser)
-        .then((user) => {
-          if (user) {
-            const { name, image, email, _id } = user;
+        .then((res) => {
+          if (res.data.user) {
+            const { name, image, email, _id } = res.data.user;
             dispatch({
               type: SET_USER,
               payload: {
@@ -165,100 +110,85 @@ export default function SignUp() {
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Logo className={classes.avatar} />
-          <Typography component="h1" variant="h3">
-            Create an account
-          </Typography>
-          <form className={classes.form}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Name"
-              name="name"
-              autoComplete="name"
-              error={nameErr !== ""}
-              autoFocus
-              helperText={nameErr}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              error={emailErr !== "" || reason !== ""}
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              helperText={emailErr}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              id="password"
-              error={passwordErr !== ""}
-              helperText={passwordErr}
-              autoComplete="current-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Re-type password"
-              type="password"
-              value={rePassword}
-              id="password2"
-              error={retypePassErr !== ""}
-              autoComplete="current-password"
-              helperText={retypePassErr}
-              onChange={(e) => setRePassword(e.target.value)}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={handleSignup}
-            >
-              Create
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="/" variant="body2">
-                  {"Already have an account? Sign In"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <div className={classes.snackbar}>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="error">
-              {reason}
-            </Alert>
-          </Snackbar>
-        </div>
-      </Grid>
-      <Grid item xs={false} sm={1} md={7} className={classes.image} />
-    </Grid>
+    <AuthPageLayout
+      page="Create an account"
+      open={open}
+      setOpen={setOpen}
+      reason={reason}
+    >
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="name"
+        label="Name"
+        name="name"
+        autoComplete="name"
+        error={nameErr !== ""}
+        autoFocus
+        helperText={nameErr}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        error={emailErr !== "" || reason !== ""}
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        helperText={emailErr}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        value={password}
+        id="password"
+        error={passwordErr !== ""}
+        helperText={passwordErr}
+        autoComplete="current-password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Re-type password"
+        type="password"
+        value={rePassword}
+        id="password2"
+        error={retypePassErr !== ""}
+        autoComplete="current-password"
+        helperText={retypePassErr}
+        onChange={(e) => setRePassword(e.target.value)}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={handleSignup}
+      >
+        Create
+      </Button>
+
+      <div>
+        <Link href="/" variant="body2">
+          {"Already have an account? Sign In"}
+        </Link>
+      </div>
+    </AuthPageLayout>
   );
 }
